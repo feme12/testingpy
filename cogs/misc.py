6ff7c4c967bf12e4e6b2cc07983d789400d74c2f
Feme12 <bobboy3454@gmail.com>
2024-09-26 04:31:41 -0500

    [Added] Added a id command

--- cogs/misc.py
+++ cogs/misc.py
@@ -1,6 +1,8 @@
 import discord
+import platform
 import json
 from discord.ext import commands
+from discord import Embed
 
 class Misc(commands.Cog):
     def __init__(self, bot):
@@ -11,6 +13,10 @@ class Misc(commands.Cog):
         latency=round(self.bot.latency, 1)
 
         await ctx.reply(f"My ping is {latency}ms")
+
+    @commands.hybrid_command(name="id", description="Returns your discord id.", with_app_command=True)
+    async def id(self, ctx: commands.Context):
+        await ctx.reply(f"**Your ID:** {ctx.author.id}")
 
 
 
